29f7ba172163c836bb5c791ba1c31cda37b01a79
Manjunath B <manjunath.b@pinelabs.com>
2023-11-20 17:25:23 +0530

    Updated code

--- src/tests/mobile/SampleMobileTest.ts
+++ src/tests/mobile/SampleMobileTest.ts
@@ -1,7 +1,7 @@
-// // src/tests/mobile/SampleMobileTest.ts
-// import { Driver } from "../../drivers/Driver";
+// src/tests/mobile/SampleMobileTest.ts
+import { Driver } from "../../drivers/Driver";
 
-// export function runMobileTest(driver: Driver): void {
-//     driver.initialize();
-//     driver.runTests();
-// }
+export function runMobileTest(driver: Driver): void {
+    driver.initialize();
+    driver.runTests();
+}
--- src/tests/web/SampleWebTest.ts
+++ src/tests/web/SampleWebTest.ts
@@ -1,7 +1,7 @@
-// // src/tests/web/SampleWebTest.ts
-// import { Driver } from "../../drivers/Driver";
+// src/tests/web/SampleWebTest.ts
+import { Driver } from "../../drivers/Driver";
 
-// export function runWebTest(driver: Driver): void {
-//     driver.initialize();
-//     driver.runTests();
-// }
+export function runWebTest(driver: Driver): void {
+    driver.initialize();
+    driver.runTests();
+}
